85bd3b0c74aef0bb1355f907a6ecc62ad796124c
Disha Talreja <dishatalreja1202@gmail.com>
2022-03-02 18:41:56 +0530

    Updated order getters(#22c8vgh)

--- src/store/modules/order/getters.ts
+++ src/store/modules/order/getters.ts
@@ -4,7 +4,7 @@ import RootState from "../../RootState";
 
 const getters: GetterTree<OrderState, RootState> = {
   getOrderItems(state){
-    return state.order.list;
+    return state.order.list.original;
   },
   
 };
