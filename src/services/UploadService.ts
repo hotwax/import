cdef0bf62bfd28d24a656e0882e094873a6419f0
Aditya Sharma <aditya.sharma@hotwax.co>
2022-03-04 10:26:15 +0530

    Implemented: service to prepare payload for upload and upload json file for import (#232z1wx) prepareUploadJsonPayload - prepared payload to be used for upload json request uploadJsonFile - uploads json file for import

---
+++ src/services/UploadService.ts
@@ -0,0 +1,32 @@
+import api from '../api'
+import { UploadRequest } from '@/types'
+
+const uploadJsonFile = async (payload: any): Promise <any>  => {
+    return api({
+      url: "uploadJsonFile",
+      method: "post",
+      ...payload
+    });
+}
+const prepareUploadJsonPayload = (request: UploadRequest) => {
+      const blob = new Blob([JSON.stringify(request.uploadData)], { type: 'application/json'});
+      const formData = new FormData();
+      const fileName =  (request.fileName ? request.fileName : Date.now() ) +".json";
+      formData.append("uploadedFile", blob, fileName);
+      if (request.params) {
+        for (const key in request.params) {
+            formData.append(key, request.params[key]);
+          }
+      }
+      return {
+          data: formData,
+          headers: {
+            'Content-Type': 'multipart/form-data;'
+        }
+      }
+}
+
+export const UploadService = {
+    prepareUploadJsonPayload,
+    uploadJsonFile
+}
