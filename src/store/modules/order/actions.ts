c45af5eba6a769f7251b70408f59a0de5ef17ae4
Disha Talreja <dishatalreja1202@gmail.com>
2022-03-30 15:19:50 +0530

    Reverted unwanted changes

--- src/store/modules/order/actions.ts
+++ src/store/modules/order/actions.ts
@@ -36,6 +36,6 @@ const actions: ActionTree<OrderState, RootState> = {
   },
   updatedOrderListItems({ commit }, orderListItems){
     commit(types.ORDER_LIST_ITEMS_UPDATED, orderListItems)
-  }
+  },
 }
 export default actions;
