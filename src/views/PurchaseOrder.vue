4a257c008883640a2768987d26029a4af949a151
Disha Talreja <dishatalreja1202@gmail.com>
2022-10-31 14:39:28 +0530

    Improved variable name(#24pp0fm)

--- src/views/PurchaseOrder.vue
+++ src/views/PurchaseOrder.vue
@@ -19,35 +19,35 @@
           <ion-list-header>{{ $t("Select the column index for the following information in the uploaded CSV.") }}</ion-list-header>
           <ion-item>
             <ion-label>{{ $t("Order ID") }}</ion-label>
-            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.orderIdField">
+            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.orderId">
                 <ion-select-option v-bind:key="index" v-for="(prop, index) in Object.keys(content[0])">{{ prop }}</ion-select-option>
             </ion-select>
           </ion-item>
 
           <ion-item>
             <ion-label>{{ $t("Shopify product SKU") }}</ion-label>
-            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.productSkuField">
+            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.productSku">
               <ion-select-option v-bind:key="index" v-for="(prop, index) in Object.keys(content[0])">{{ prop }}</ion-select-option>
             </ion-select>
           </ion-item>
 
           <ion-item>
             <ion-label>{{ $t("Arrival date") }}</ion-label>
-            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.dateField">
+            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.date">
               <ion-select-option v-bind:key="index" v-for="(prop, index) in Object.keys(content[0])">{{ prop }}</ion-select-option>
             </ion-select>
           </ion-item>
 
           <ion-item>
             <ion-label>{{ $t("Ordered quantity") }}</ion-label>
-            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.quantityField">
+            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.quantity">
               <ion-select-option v-bind:key="index" v-for="(prop, index) in Object.keys(content[0])">{{ prop }}</ion-select-option>
             </ion-select>
           </ion-item>
 
           <ion-item>
             <ion-label>{{ $t("Facility ID") }}</ion-label>
-            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.facilityField">
+            <ion-select interface="popover" v-if="content.length" :placeholder = "$t('Select')" v-model="fields.facility">
               <ion-select-option v-bind:key="index" v-for="(prop, index) in Object.keys(content[0])">{{ prop }}</ion-select-option>
             </ion-select>
           </ion-item>
@@ -93,11 +93,11 @@ export default defineComponent({
         file: "",
         content: [],
         fields:{
-          orderIdField: "",
-          productSkuField: "",
-          dateField: "",
-          quantityField: "",
-          facilityField: "",
+          orderId: "",
+          productSku: "",
+          date: "",
+          quantity: "",
+          facility: "",
         },
         orderItemsList: [],
       }
@@ -126,11 +126,11 @@ export default defineComponent({
           showToast(translate("Please upload a valid purchase order csv to continue"));
         } else if (!areAllFieldsSelected) {
           this.orderItemsList = this.content.map(item => ({
-            orderId: item[this.fields.orderIdField],
-            shopifyProductSKU: item[this.fields.productSkuField],
-            arrivalDate: item[this.fields.dateField],
-            quantityOrdered: item[this.fields.quantityField],
-            externalFacilityId: item[this.fields.facilityField]
+            orderId: item[this.fields.orderId],
+            shopifyProductSKU: item[this.fields.productSku],
+            arrivalDate: item[this.fields.date],
+            quantityOrdered: item[this.fields.quantity],
+            externalFacilityId: item[this.fields.facility]
           }))
           this.store.dispatch('order/updatedOrderList', this.orderItemsList);
           this.router.push({
