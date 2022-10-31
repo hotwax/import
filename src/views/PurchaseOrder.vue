35a350d96a0237edc585c7562e5af7c2525ba225
Disha Talreja <dishatalreja1202@gmail.com>
2022-10-31 13:20:29 +0530

    Improved code(#24pp0fm)

--- src/views/PurchaseOrder.vue
+++ src/views/PurchaseOrder.vue
@@ -125,15 +125,13 @@ export default defineComponent({
         if (this.content.length <= 0) {
           showToast(translate("Please upload a valid purchase order csv to continue"));
         } else if (!areAllFieldsSelected) {
-          this.orderItemsList = this.content.map(item => {
-            return {
-              orderId: item[this.fields.orderIdField],
-              shopifyProductSKU: item[this.fields.productSkuField],
-              arrivalDate: item[this.fields.dateField],
-              quantityOrdered: item[this.fields.quantityField],
-              externalFacilityId: item[this.fields.facilityField]
-            }
-          })
+          this.orderItemsList = this.content.map(item => ({
+            orderId: item[this.fields.orderIdField],
+            shopifyProductSKU: item[this.fields.productSkuField],
+            arrivalDate: item[this.fields.dateField],
+            quantityOrdered: item[this.fields.quantityField],
+            externalFacilityId: item[this.fields.facilityField]
+          }))
           this.store.dispatch('order/updatedOrderList', this.orderItemsList);
           this.router.push({
             name:'PurchaseOrderDetail'
