91003a89d1806535e099425f435f28baac49060a
Yash Maheshwari <yash.maheshwari@hotwax.co>
2023-01-06 11:16:28 +0530

    Removed: unwanted code for search page(#85zrhdn0f)

--- src/store/modules/product/actions.ts
+++ src/store/modules/product/actions.ts
@@ -39,43 +39,6 @@ const actions: ActionTree<ProductState, RootState> = {
     // TODO Handle specific error
     return resp;
   },
-
-  // Find Product
-  async findProduct ({ commit, state }, payload) {
-
-    // Show loader only when new query and not the infinite scroll
-    if (payload.viewIndex === 0) emitter.emit("presentLoader");
-
-    let resp;
-
-    try {
-      resp = await ProductService.fetchProducts({
-        // used sku as we are currently only using sku to search for the product
-        "filters": ['sku: ' + payload.queryString],
-        "viewSize": payload.viewSize,
-        "viewIndex": payload.viewIndex
-      })
-
-      // resp.data.response.numFound tells the number of items in the response
-      if (resp.status === 200 && resp.data.response.numFound > 0 && !hasError(resp)) {
-        let products = resp.data.response.docs;
-        const totalProductsCount = resp.data.response.numFound;
-
-        if (payload.viewIndex && payload.viewIndex > 0) products = state.products.list.concat(products)
-        commit(types.PRODUCT_SEARCH_UPDATED, { products: products, totalProductsCount: totalProductsCount })
-      } else {
-        //showing error whenever getting no products in the response or having any other error
-        showToast(translate("Product not found"));
-      }
-      // Remove added loader only when new query and not the infinite scroll
-      if (payload.viewIndex === 0) emitter.emit("dismissLoader");
-    } catch(error){
-      console.error(error)
-      showToast(translate("Something went wrong"));
-    }
-    // TODO Handle specific error
-    return resp;
-  },
 }
 
 export default actions;
