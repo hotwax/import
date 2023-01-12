63235f473174a614e76393b71bf85d70d029bb06
Disha Talreja <dishatalreja1202@gmail.com>
2023-01-12 15:16:01 +0530

    Added package.json and package-lock.json files(#31nm26y)

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import '@hotwax/apps-theme/src/hc-theme.css';
+import '@hotwax/apps-theme/css/theme.css';
 
 import i18n from './i18n'
 import store from './store'
