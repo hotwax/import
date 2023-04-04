beb40a45a1afd282d722b4164f67c75a391f0a23
k2maan <ketuman333@gmail.com>
2023-04-04 16:58:38 +0530

    Fixed: updated this.content to be object (#85zrv53ft)

--- src/views/PurchaseOrder.vue
+++ src/views/PurchaseOrder.vue
@@ -117,7 +117,7 @@ export default defineComponent({
             showToast(translate("No new file upload. Please try again"));
           }
         } catch {
-          this.file = this.content = {}
+          this.content = []
           showToast(translate("Please upload a valid purchase order csv to continue"));
         }
       },
