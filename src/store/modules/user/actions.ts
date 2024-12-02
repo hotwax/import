2e4c78ef04b9eeea8b90046cf2404757794e1ec1
Ravi Lodhi <ravi.lodhi@hotwaxsystems.com>
2024-12-02 18:30:43 +0530

    Fixed: Corrected unwanted change (#166).

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -228,7 +228,7 @@ const actions: ActionTree<UserState, RootState> = {
       const params = {
         mappingPrefName: payload.name,
         mappingPrefValue: JSON.stringify(payload.value),
-        mappingPrefTypeEnumId: "TT"
+        mappingPrefTypeEnumId
       }
 
       const resp = await UserService.createFieldMapping(params);
