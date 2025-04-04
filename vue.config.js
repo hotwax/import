3e13ebb725922c6634cab2dbf514d5f3b74d7d3b
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-04-04 16:17:02 +0530

    Improved: pwa support in the app.(dxp/379)

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 const path = require('path')
 require("@hotwax/app-version-info")
 module.exports = {
+  pwa: {
+    name: "Import - HotWax Commerce",
+    themeColor: "#FFFFFF",
+    manifestOptions: {
+      short_name: "Import",
+      start_url: "./"
+    },
+    id: "/",
+    display: "standalone",
+    background_color: "#000000"
+  },
   configureWebpack: {
     resolve: {
       alias: {
